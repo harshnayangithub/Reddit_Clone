5646e68a7212b0a0a4c0f9136d62db633aeebc3c
Harsh Nayan <126700987+harshnayangithub@users.noreply.github.com>
2024-03-16 13:35:52 +0530

    Update clientApp.ts

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+itimport { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBqe95nCUomWkU9PnZT_OlV_NCAZnTmyz0",
   authDomain: "reddit-clone-r.firebaseapp.com",
   projectId: "reddit-clone-r",
-  storageBucket: "reddit-clone-r.appspot.com",
+  storageBucket: "redditclone.appspot.com",
   messagingSenderId: "233455437474",
   appId: "1:233455437474:web:5f21ac4b94a42fddb379fb",
   measurementId: "G-3L9HMZR4XQ"
